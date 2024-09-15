71d8840d16aa8388245a71ccdc44e63f9e41a754
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-09-15 20:02:49 +0530

    added Unique constraint

--- DDL/p3_unique.sql
+++ DDL/p3_unique.sql
@@ -16,3 +16,26 @@ CREATE TABLE users(
     password VARCHAR(20)
 );
 -- After running the above query you will see that the 'password' column does not have a UNIQUE constraint
+
+
+-- We also have a different method of adding constraints in a table
+
+-- The syntax for adding a UNIQUE constraint is as follows
+-- CREATE TABLE table_name(
+--     column1_name column1_datatype,
+--     column2_name column2_datatype,
+--     column3_name column3_datatype,
+--     ...
+--     CONSTRAINT user_email_unique UNIQUE(email)
+-- );
+
+-- The above query can also be used to add a constraint
+-- Lets take a scenario where the combination of name and email should be unique then in that case we can
+-- use the following query
+-- CREATE TABLE users(
+--     user_id INT,
+--     name VARCHAR(20),
+--     email VARCHAR(50),
+--     password VARCHAR(20),
+--     CONSTRAINT user_name_email_unique UNIQUE(name, email)
+-- );
